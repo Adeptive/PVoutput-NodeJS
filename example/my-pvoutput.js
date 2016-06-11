b3fa2975d384b48c93e44a950ef013f34c0dfb40
Peter Peerdeman <peter@lifely.nl>
2016-06-11 20:57:26 +0200

    refactor(example): update example with getStatus and getOutput calls in one single file

--- example/my-pvoutput.js
+++ example/my-pvoutput.js
@@ -7,4 +7,31 @@ var pvoutput = new PVoutputAPI({
     systemId: "xxx"
 });
 
-module.exports = pvoutput;
+// Example sending data
+var moment = require('moment');
+var now = moment().utc().valueOf();
+var timestamp = now;
+var solarWatt = 6000;
+var solarWattHour = 400;
+var consumptionWatt = 12000;
+var consumptionWattHour = 650;
+
+pvoutput.send(timestamp, solarWatt, solarWattHour, consumptionWatt, consumptionWattHour, function(statusCode, body) {
+    console.log(body);
+});
+
+// Example: get status
+pvoutput.getStatus().then(function(results) {
+    console.log(results);
+})
+.catch(function(err) {
+    console.log(err);
+});
+
+//Example: get outputs
+pvoutput.getOutput().then(function(results) {
+    console.log(results);
+})
+.catch(function(err) {
+    console.log(err);
+});
